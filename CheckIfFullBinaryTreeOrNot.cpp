8c1ade60de3a705834bf8e1b0c76dd5fed1ef09b
PeacocksFly <peacocks01@gmail.com>
2018-02-15 12:34:48 +0100

    Solution to -check whether the binary tree is full- from GeeksforGeeks

---
+++ CheckIfFullBinaryTreeOrNot.cpp
@@ -0,0 +1,39 @@
+#include "CheckIfFullBinaryTreeOrNot.h"
+
+
+CheckIfFullBinaryTreeOrNot::CheckIfFullBinaryTreeOrNot() {
+
+	head = new node(1);
+	head->l = new node(2);
+	head->r = new node(3);
+	head->r->r = new node(4);
+	head->r->l = new node(4);
+	head->l = new node(2);
+}
+
+
+CheckIfFullBinaryTreeOrNot::~CheckIfFullBinaryTreeOrNot() {
+
+	clearTreeR(head);
+}
+
+void CheckIfFullBinaryTreeOrNot::clearTreeR(link h) {
+
+	if (!h) return;
+	link l = nullptr, r = nullptr;
+	if (h->l) l = h->l;
+	if (h->r) r = h->r;
+	delete h;
+	clearTreeR(l);
+	clearTreeR(r);
+}
+
+int CheckIfFullBinaryTreeOrNot::isFullBinaryTreeR(link h) {
+
+	if (!h) return -1;
+
+	int u = isFullBinaryTreeR(h->l);
+	int v = isFullBinaryTreeR(h->r);
+
+	return ( u == 0 || v == 0 || (u * v) == -1)? 0 : 1;
+}
---
+++ CheckIfFullBinaryTreeOrNot.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+//A full binary tree is defined as a binary tree in which all nodes have either zero or two child nodes.
+//Conversely, there is no node in a full binary tree, which has one child node.
+//More information about full binary trees can be found here.
+#include <iostream>
+class CheckIfFullBinaryTreeOrNot {
+
+private:
+	struct node {
+		int val;
+		node* l;
+		node* r;
+		node(int _val) :
+			val(_val), l(nullptr), r(nullptr) {}
+	};
+
+	typedef node* link;
+	link head;
+	void clearTreeR(link);
+	int isFullBinaryTreeR(link);
+
+public:
+	CheckIfFullBinaryTreeOrNot();
+	~CheckIfFullBinaryTreeOrNot();
+	int isFullBinaryTree() { return isFullBinaryTreeR(head); };
+
+};
