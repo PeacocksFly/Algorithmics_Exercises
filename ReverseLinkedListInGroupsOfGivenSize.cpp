95f40b7b63ad42bcf94e1b4168d61952151f01e7
PeacocksFly <peacocks01@gmail.com>
2018-02-28 11:15:39 +0100

    Solution to how to reverse a linked list by groups of given from geeksforgeeks

---
+++ ReverseLinkedListInGroupsOfGivenSize.cpp
@@ -0,0 +1,60 @@
+#include "ReverseLinkedListInGroupsOfGivenSize.h"
+
+ReverseLinkedListInGroupsOfGivenSize::~ReverseLinkedListInGroupsOfGivenSize() {
+	link t = _head;
+	link g = nullptr;
+	while (t != nullptr) {
+		g = t;
+		t = t->next;
+		delete g;
+	}
+}
+
+
+void ReverseLinkedListInGroupsOfGivenSize::reverse(int k) {
+	
+	link s = _head; 
+	if (!s) return;
+
+	link g = s;
+	link sPrev = s;
+	link i = nullptr;
+	link j = nullptr;
+	int kCopy = k;
+	int a = 0;
+
+	do {
+		i = g->next;
+		while (--k > 0 && i) {
+			j = i->next;
+			i->next = g;
+			g = i;
+			i = j;
+		}
+		if (a == 0) {
+			_head = g;
+			a++;
+		}
+		else {
+			sPrev->next = g;
+		}
+		s->next = i;
+		sPrev = s;
+		s = i;
+		g = i;
+		k = kCopy;
+		
+	} while (s);
+}
+
+std::ostream& operator<<(std::ostream& os, const ReverseLinkedListInGroupsOfGivenSize& list) {
+
+	ReverseLinkedListInGroupsOfGivenSize::link t = list._head;
+	while (t != nullptr) {
+		os << t->value;
+		t = t->next;
+	}
+	return os;
+}
+
+
---
+++ ReverseLinkedListInGroupsOfGivenSize.h
@@ -0,0 +1,34 @@
+#pragma once
+
+//Given a linked list, write a function to reverse every k nodes(where k is an input to the function).
+//
+//Example:
+//Inputs:  1->2->3->4->5->6->7->8->NULL and k = 3
+//	Output : 3->2->1->6->5->4->8->7->NULL.
+//
+//	Inputs : 1->2->3->4->5->6->7->8->NULL and k = 5
+//	Output : 5->4->3->2->1->8->7->6->NULL.
+
+#include <iostream>
+
+class ReverseLinkedListInGroupsOfGivenSize {
+
+private:
+	struct node {
+		node* next;
+		int value;
+		node(int _value, node* _next) :
+			value(_value),
+			next(_next) {}
+	};
+
+	typedef node* link;
+	link _head;
+
+public:
+	ReverseLinkedListInGroupsOfGivenSize() : _head(nullptr) {}
+	~ReverseLinkedListInGroupsOfGivenSize();
+	void insert(int _value) { _head = new node(_value, _head); };
+	void reverse(int);
+	friend std::ostream& operator<<(std::ostream&, const ReverseLinkedListInGroupsOfGivenSize&);
+};
